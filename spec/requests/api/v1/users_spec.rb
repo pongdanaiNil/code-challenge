8e262861633ac405a70e915cceba131a685ea27b
Captain <pongdanai.ni@gmail.com>
2023-03-06 11:58:54 +0700

    Remove unused spec

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,28 +6,6 @@ RSpec.describe "Api::V1::Users", type: :request do
   let!(:token)          { FactoryBot.create(:access_token, application:, resource_owner_id: user.id) }
   let!(:headers)        { { Authorization: "Bearer #{token.token}" } }
 
-  describe 'GET /me' do
-    it 'should get current user correctly' do
-      get '/api/v1/users/me', headers: headers
-
-      result = JSON.parse(response.body)
-      expect(response.status).to eq(200)
-      expect(result['user']).to eq(user.as_json)
-    end
-
-    it 'should unauthorized with invalid user token' do
-      get '/api/v1/users/me', headers: { Authorization: 'Bearer token' }
-
-      expect(response.status).to eq(401)
-    end
-
-    it 'should render 401 when authorize header' do
-      get '/api/v1/users/me'
-
-      expect(response.status).to eq(401)
-    end
-  end
-
   describe "Post /registration" do
     it 'should create new user correctly' do
 
